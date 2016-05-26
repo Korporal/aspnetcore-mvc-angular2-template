5fb79c682d21940f1f67d1ea217caca8995e61dd
scott <dakota148@hotmail.com>
2016-05-26 13:39:26 -0700

    angular 2 - working in browser

---
+++ app/main.ts
@@ -0,0 +1,27 @@
+﻿
+import {Component, OnInit} from 'angular2/core';
+import {RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS} from 'angular2/router';
+import {COMMON_DIRECTIVES} from 'angular2/common';
+
+@Component({
+    selector: 'main-component',
+    template: `
+        <h1>{{title}}</h1>
+        `,
+    directives: [ROUTER_DIRECTIVES],
+    providers: [ROUTER_PROVIDERS]
+})
+
+
+@RouteConfig([])
+
+
+export class MainComponent implements OnInit {
+    title: string = '! my new component !';
+
+    ngOnInit() {
+        console.log('@ main component oninit');
+        
+    }
+}
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,16 @@
 const gulp = require('gulp'),
     del = require('del'),
     typescript = require('gulp-typescript'),
-    tsconfig = require('./tsconfig.json'),
     sourcemaps = require('gulp-sourcemaps'),
     tslint = require('gulp-tslint'),
     browserSync = require('browser-sync'),
     concat = require('gulp-concat'),
     less = require('gulp-less');
 
+var tsproject = typescript.createProject('tsconfig.json');
+
 const base = {
-    source:'app',
+    source:'.',
     build: 'wwwroot',
     node: 'node_modules',
     bower:'bower_modules'
@@ -19,17 +20,19 @@ const paths = {
     source: {
         bower: base.bower,
 
-        app: base.source + '/**/*.ts',
+        app: base.source + '/app/**/*.ts',
         html: [base.source + '/**/*.html', '!' + base.source + '/index.html'],
         index: base.source+'/index.html'
     },
     build: {
         root: base.build,
+        app: base.build + '/app',
         css: base.build + '/css',
         fonts: base.build + '/fonts',
         libs: base.build + '/libs'
     },
     libs: {
+        base:base.node,
         angular:base.node + '/angular2',
         es6: base.node + '/es6-shim',
         system: base.node + '/systemjs',
@@ -54,7 +57,8 @@ gulp.task('clean', ['clean-app', 'clean-css', 'clean-fonts']);
 gulp.task('setup-css', function () {
 
     gulp.src([
-            paths.source.bower + '/bootstrap/dist/css/**/*.css'
+            paths.source.bower + '/bootstrap/dist/css/**/*.css',
+            paths.source.bower + '/font-awesome/css/**/*.css'
         ])
         .pipe(gulp.dest(paths.build.css));
 
@@ -80,9 +84,11 @@ gulp.task('setup-libs', function () {
             paths.libs.angular + '/bundles/angular2-polyfills.js',
             paths.libs.angular + '/bundles/http.*.js',
             paths.libs.angular + '/bundles/router.*.js',
+            paths.libs.angular + '/platform/browser.js',
             paths.libs.es6 + '/es6-shim.min.js',
             paths.libs.angular + '/es6/dev/src/testing/shims_for_IE.js',
             paths.libs.system + '/dist/*.*',
+            paths.libs.base + 'rxjs/**',
             paths.libs.rxjs + '/bundles/Rx.js',
 
         ])
@@ -92,14 +98,19 @@ gulp.task('setup-libs', function () {
 gulp.task('transpile', function () {
 
     var result = gulp.src([
-            paths.source.app + '/**/*'
+            paths.libs.angular + '/bundles/typings/angular2/angular2.d.ts',
+            paths.libs.angular + '/bundles/typings/angular2/http.d.ts',
+            paths.libs.angular + '/bundles/typings/angular2/router.d.ts',
+            //paths.libs.base + 'typescript-compiler/lib/lib.es6.d.ts',
+            //"node_modules/@reactivex/rxjs/dist/es6/Rx.d.ts',
+            "app/**/*.ts"
         ])
         .pipe(sourcemaps.init())
-        .pipe(typescript(tsconfig));
+        .pipe(typescript(tsproject));
 
     result.js
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest(paths.build.libs));
+        .pipe(gulp.dest(paths.build.app));
 });
 
 
