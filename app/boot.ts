39982e3961a7ad537c96bc2ec504677b0fa25f4a
scott <dakota148@hotmail.com>
2016-05-26 13:52:01 -0700

    angular 2 setup - minor systemjs changes

--- app/boot.ts
+++ app/boot.ts
@@ -1,9 +1,6 @@
 ﻿///<reference path="../node_modules/angular2/typings/browser.d.ts"/>
-
 import { bootstrap } from 'angular2/platform/browser';
- 
 import { HTTP_PROVIDERS } from 'angular2/http';
-
 import { MainComponent } from './main';
 
 bootstrap(MainComponent, [HTTP_PROVIDERS]);
